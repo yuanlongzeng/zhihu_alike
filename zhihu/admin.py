a283106ca9d1e08644024fbab2b15eee625db086
yuanlongzeng <dengluylz@gmail.com>
2018-05-21 12:38:46 +0800

    xadmin后台通用配置

--- zhihu/admin.py
+++ zhihu/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+import xadmin
+from xadmin import forms, views
+
+# Register your models here.这样可以在后台进行显示、管理
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ['title', 'created_time', 'modified_time', 'category', 'user']
+#
+#
+# admin.site.register(Post, PostAdmin)
+# admin.site.register(Category)
+# admin.site.register(Tag)
+
+class BaseSetting:
+    enable_themes = True  # 设置主题
+    use_bootswatch = True
+
+
+class GlobalSetting:
+    site_title = "XX社交只是分享网站"  # 设置主题
+    site_footer = "xx social website for knowledge"
+    menu_style = "accordion"  #每个app下的model可展开-收缩
+
+
+
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView,GlobalSetting)
+
+#xadmin.site.register(UserProfile, UserProfileAdmin)
